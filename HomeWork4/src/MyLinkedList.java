cd5c032b00c98e7391f257e2a4b3da358658470c
Витек Сергеев <vitkans@Noutbuk-Vitek.local>
2023-12-04 00:15:08 +0300

    Add home4 new

---
+++ HomeWork4/src/MyLinkedList.java
@@ -0,0 +1,144 @@
+import java.util.Collection;
+import java.util.Iterator;
+
+public class MyLinkedList<E> implements Iterable<E> {
+    private Node<E> head;
+    private int size;
+
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public void add(E data) {
+        Node<E> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public void add(int index, E data) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> newNode = new Node<>(data);
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node<E> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        E data;
+        if (index == 0) {
+            data = head.data;
+            head = head.next;
+        } else {
+            Node<E> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            data = current.next.data;
+            current.next = current.next.next;
+        }
+        size--;
+        return data;
+    }
+
+    public boolean addAll(Collection<? extends E> c) {
+        boolean modified = false;
+        for (E e : c) {
+            add(e);
+            modified = true;
+        }
+        return modified;
+    }
+
+    public boolean copy(Collection<? super E> c) {
+        boolean modified = false;
+        for (E e : this) {
+            c.add(e);
+            modified = true;
+        }
+        return modified;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private Node<E> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new java.util.NoSuchElementException();
+                }
+                E data = current.data;
+                current = current.next;
+                return data;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
+}
